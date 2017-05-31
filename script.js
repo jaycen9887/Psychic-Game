003621fcc79bcc9ea86075c6cc5b114251c79afb
Jaycen <jaycen9887@gmail.com>
2017-05-31 10:41:40 -0400

    Game

---
+++ script.js
@@ -0,0 +1,80 @@
+var key;
+var wins = 0;
+var losses = 0;
+var guessesRemain = 10;
+var guesses = "";
+var hint = "";
+var cmp = "";
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var winCount = document.getElementById("wins");
+var loseCount = document.getElementById("losses");
+var guesscount = document.getElementById("guessesRemain");
+var guessesMade = document.getElementById("guesses");
+var hints = document.getElementById("hint");
+
+document.onkeyup = function(event) {
+    key = event.key;
+    
+    if(guessesRemain > 0) {
+        
+        if (key.length === 1 && key.match(/[a-z]/i)) {
+
+            if (!guesses.includes(key)) {
+                guesses += (" " + key);
+                guessesMade.textContent = guesses;
+                if (checkGuess(key)) {
+                    wins++;
+                    winCount.textContent = wins;
+                    playGame();
+                }
+            }
+
+
+        }
+        guessesRemain--;
+        guesscount.textContent = guessesRemain;
+    } else {
+        losses++;
+        loseCount.textContent = losses;
+        playGame();
+    }
+    
+}
+
+var randomLetter = function() {
+    var max = letters.length;
+    var random = Math.floor(Math.random() * (max - 0) + 0);
+    
+    return letters[random];    
+}
+
+var checkGuess = function(guess) {
+    if (guess === cmp){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+var playGame = function(){
+    cmp = randomLetter();
+    resetGame();
+}
+
+var resetGame = function() {
+    
+    guessesRemain = 10;
+    guesses = "";
+    hint = "";
+    
+    winCount.textContent = wins;
+    loseCount.textContent = losses;
+    guesscount.textContent = guessesRemain;
+    guessesMade.textContent = guesses;
+    hints.textContent = hint;
+}
+
+
+playGame();
